c54d3f71b387e421d3db7ebbe746f3ede55951c8
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-07-04 01:14:08 +0200

    button stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+/* eslint-disable */
+
+import { ThemeProvider } from 'styled-components';
+import { addDecorator } from '@storybook/react';
+import { withThemes } from '@react-theming/storybook-addon';
+import { defaultTheme } from "../src/styles/theme/default-theme";
+
+
+// pass ThemeProvider and array of your themes to decorator
+addDecorator(withThemes(ThemeProvider, [defaultTheme]));
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
