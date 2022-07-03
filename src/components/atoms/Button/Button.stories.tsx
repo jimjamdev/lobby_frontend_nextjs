5a8b219e40a894a11b427521d663921819839a80
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-07-04 01:43:56 +0200

    button stories args

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -10,8 +10,9 @@ export default {
   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
   * to learn how to generate automatic titles
   */
-  title: 'Button',
+  title: 'Design System/Atoms/Button',
   component: Button,
+  storyName: 'Button',
   argTypes: {
     variant: {
       options: ['primary', 'secondary', 'alternative'],
