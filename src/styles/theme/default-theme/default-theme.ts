3ddf3a68333a7cb1641407456f3b554002efae53
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-07-07 00:59:08 +0200

    revert theme

--- src/styles/theme/default-theme/default-theme.ts
+++ src/styles/theme/default-theme/default-theme.ts
@@ -1,19 +1,9 @@
 import { buttonSizesTheme, buttonsTheme } from './buttons';
-// import { colorsTheme } from './colors';
+import { colorsTheme } from './colors';
 
-const theme: any = {
-  colors: {
-    bullshit: 'brown',
-    mono: ['#f2f2f2', '#d9d9d9', '#bfbfbf', '#a6a6a6', '#8c8c8c', '#737373', '#595959', '#404040', '#262626', '#0d0d0d'],
-    primary: ['#e5fffc', '#b3fff5', '#80ffef', '#4dffe8', '#1affe2', '#00e6c8', '#00b39c', '#00806f', '#004d43', '#001a16'],
-    secondary: ['#feefe6', '#fdcfb4', '#fcae83', '#fb8e51', '#f96e1f', '#e05406', '#ae4104', '#7c2f03', '#4b1c02', '#190901'],
-    highlight: ['#ffffe5', '#ffffb3', '#ffff80', '#ffff4d', '#ffff4d', '#e5e600', '#b2b300', '#7f8000', '#4c4d00', '#191a00'],
-    warning: ['#ffffe5', '#ffffb3', '#ffff80', '#ffff4d', '#ffff4d', '#e5e600', '#b2b300', '#7f8000', '#4c4d00', '#191a00'],
-    error: ['#ffe9e5', '#ffbeb3', '#ff9380', '#ff684d', '#ff3c1a', '#e62300', '#b31b00', '#801300', '#4d0c00', '#1a0400'],
-    info: ['#e6f7fe', '#b5e6fc', '#84d5fb', '#53c4f9', '#21b3f7', '#0899de', '#0677ac', '#04557b', '#03334a', '#011119'],
-    success: ['#e5fff3', '#b3ffdb', '#80ffc4', '#4dffac', '#1aff94', '#00e67a', '#00b35f', '#008044', '#004d29', '#001a0e'],
-  },
-};
+const theme: any = {};
+
+theme.colors = colorsTheme();
 theme.buttons = buttonsTheme({ colors: theme.colors });
 theme.buttonSizes = buttonSizesTheme();
 
