b87b06a18993873b23387b9a29b0131b0839ef45
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-07-09 00:23:12 +0200

    nextjs locales

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- i18n: {
-   locales: ['en', 'fr', 'nl'],
-   defaultLocale: 'en'
+  i18n: {
+   locales: ['en', 'fr', 'nl', 'sv'],
+   defaultLocale: 'en',
+   localeDetection: true
  },
   reactStrictMode: true,
   compiler: {
