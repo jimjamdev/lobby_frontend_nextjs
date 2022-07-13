a526ce7b445e18ad5a762fc38135d4a01cffd67b
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-07-14 01:10:25 +0200

    build fixes

--- src/components/templates/DefaultTemplate/DefaultTemplateContainer/DefaultTemplateContainer.tsx
+++ src/components/templates/DefaultTemplate/DefaultTemplateContainer/DefaultTemplateContainer.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const DefaultTemplateContainer = styled.main`
   display: grid;
-  grid-template-areas: "headerArea" "topNavArea" "contentArea" "footerArea" "mobileNavArea";
+  grid-template-areas: "headerArea headerArea headerArea" "topNavArea topNavArea topNavArea" "contentArea contentArea contentArea" "footerArea footerArea footerArea" "mobileNavArea mobileNavArea mobileNavArea";
   grid-template-rows: auto auto 1fr auto auto;
   min-height: 100%;
 `;
